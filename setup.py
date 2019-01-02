29fac1b27a5d7bc0ce60103522b67c456d8e03f5
Jeff Triplett <jeff.triplett@gmail.com>
2019-01-02 09:14:21 -0600

    :shirt: More linting with black

--- setup.py
+++ setup.py
@@ -12,20 +12,20 @@ from setuptools import find_packages, setup, Command
 from shutil import rmtree
 
 # Package meta-data.
-NAME = 'standup'
-DESCRIPTION = 'Removes the pain of using Toggl with Geekbot'
-URL = 'https://gist.github.com/jefftriplett/2dce2823fb9dc8dbbdc34160d41e0bb4'
-EMAIL = 'jeff.triplett@gmail.com'
-AUTHOR = 'Jeff Triplett'
+NAME = "standup"
+DESCRIPTION = "Removes the pain of using Toggl with Geekbot"
+URL = "https://gist.github.com/jefftriplett/2dce2823fb9dc8dbbdc34160d41e0bb4"
+EMAIL = "jeff.triplett@gmail.com"
+AUTHOR = "Jeff Triplett"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'click',
-    'click-default-group',
-    'crayons',
-    'humanfriendly',
-    'maya',
-    'togglwrapper',
+    "click",
+    "click-default-group",
+    "crayons",
+    "humanfriendly",
+    "maya",
+    "togglwrapper",
 ]
 
 # The rest you shouldn't have to touch too much :)
@@ -37,25 +37,25 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 # Import the README and use it as the long-description.
 # Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
-with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = '\n' + f.read()
+with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
 
 # Load the package's __version__.py module as a dictionary.
 about = {}
-with open(os.path.join(here, NAME, '__version__.py')) as f:
+with open(os.path.join(here, NAME, "__version__.py")) as f:
     exec(f.read(), about)
 
 
 class UploadCommand(Command):
     """Support setup.py upload."""
 
-    description = 'Build and publish the package.'
+    description = "Build and publish the package."
     user_options = []
 
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         pass
@@ -65,16 +65,16 @@ class UploadCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
         except OSError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
 
-        self.status('Uploading the package to PyPi via Twine…')
-        os.system('twine upload dist/*')
+        self.status("Uploading the package to PyPi via Twine…")
+        os.system("twine upload dist/*")
 
         sys.exit()
 
@@ -82,39 +82,34 @@ class UploadCommand(Command):
 # Where the magic happens:
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=about["__version__"],
     description=DESCRIPTION,
     long_description=long_description,
     author=AUTHOR,
     author_email=EMAIL,
     url=URL,
-    packages=find_packages(exclude=('tests',)),
+    packages=find_packages(exclude=("tests",)),
     # If your package is a single module, use this instead of 'packages':
     # py_modules=['mypackage'],
-
-    entry_points={
-        'console_scripts': ['standup=standup:cli'],
-    },
+    entry_points={"console_scripts": ["standup=standup:cli"]},
     install_requires=REQUIRED,
     include_package_data=True,
-    license='MIT',
+    license="MIT",
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
     # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
+    cmdclass={"upload": UploadCommand},
 )
