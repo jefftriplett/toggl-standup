df88c46f96da3da869ec8c808e069d293cb27df9
Jeff Triplett <jeff.triplett@gmail.com>
2019-01-02 09:13:12 -0600

    :calendar: Adds support for identifying multiple days

--- standup/cli.py
+++ standup/cli.py
@@ -38,17 +38,21 @@ def main(slang_date, show_time):
 
     now = maya.when(slang_date, timezone='US/Central')
     now = now.datetime().replace(hour=6, minute=0, second=0, microsecond=0)
-
     now = maya.MayaDT.from_datetime(now)
-    click.echo(
-        crayons.green(
-            '## {0}'.format(now.slang_date())
-        )
-    )
-
     time_entries = toggl.TimeEntries.get(start_date=now.iso8601())
 
+    last_start_slang = None
     for time_entry in time_entries:
+        start = maya.when(time_entry['start'])
+        start_slag = start.slang_date()
+        if start_slag != last_start_slang:
+            click.echo(
+                crayons.green(
+                    '## {0}'.format(start.slang_date())
+                )
+            )
+        last_start_slang = start_slag
+
         project_id = time_entry.get('pid')
         if project_id:
             project = toggl.Projects.get(project_id)
@@ -58,19 +62,19 @@ def main(slang_date, show_time):
 
         if show_time:
             click.echo(
-                '- [{0}] {1} ({2}){3}'.format(
-                    project_name,
-                    time_entry['description'],
-                    format_timespan(time_entry['duration']),
-                    ' :moneybag:' if time_entry['billable'] else ''
+                '- [{project_name}] {description} ({duration}){billable}'.format(
+                    project_name=project_name,
+                    description=time_entry['description'],
+                    duration=format_timespan(time_entry['duration']),
+                    billable=' :moneybag:' if time_entry['billable'] else ''
                 )
             )
         else:
             click.echo(
-                '- [{0}] {1}{2}'.format(
-                    project_name,
-                    time_entry['description'],
-                    ' :moneybag:' if time_entry['billable'] else ''
+                '- [{project_name}] {description}{billable}'.format(
+                    project_name=project_name,
+                    description=time_entry['description'],
+                    billable=' :moneybag:' if time_entry['billable'] else ''
                 )
             )
 
